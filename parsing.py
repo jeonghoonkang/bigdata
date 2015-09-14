1a5053c28700af72750b497d77a913a3c3be90e7
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2015-09-14 09:29:39 +0900

    Update parsing.py

--- parsing.py
+++ parsing.py
@@ -11,32 +11,12 @@ print "loading ... Bin file"
 print tsfile
 print 
 
-print "keys of %s" %filename
-print tsfile.keys()
-print
-
-print "tsfile['ts'][0]"
-print tsfile['ts'][0]
-print
-
-print "tsfile['ts'][1]"
-print tsfile['ts'][1]
-print
-
-print "tsfile['ts'][2]"
-print tsfile['ts'][2]
-print
-
-print "tsfile['ts'][10]"
-print tsfile['ts'][10]
-print
-
-print "tsfile['value'][0]"
-print tsfile['value'][0]
-print
-
-print "tsfile['value'][-1]"
-print tsfile['value'][-1]
-print
+print "keys of %s" %filename, tsfile.keys()
+print "tsfile['ts'][0]", tsfile['ts'][0]
+print "tsfile['ts'][1]", tsfile['ts'][1]
+print "tsfile['ts'][2]", tsfile['ts'][2]
+print "tsfile['ts'][10]", tsfile['ts'][10]
+print "tsfile['value'][0]", tsfile['value'][0]
+print "tsfile['value'][-1]", tsfile['value'][-1]
 
 print "length of key ts is %d" % len(tsfile['ts'])
